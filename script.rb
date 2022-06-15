623abf48f8e4e380ed35d13f53563241a1980351
Roman Geathers <romangeathersdev@gmail.com>
2022-06-15 13:15:26 -0400

    Write method to get player choice & update board
    Write a new method that gets the location choice from the player
    and then updates the new board

--- script.rb
+++ script.rb
@@ -10,6 +10,10 @@ class Board
       p @board_array[i]
     end
   end
+
+  def place_choice(player_location)
+    @board_array[player_location[0]][player_location[1]] = "X"
+  end
 end	
 
 class Player
@@ -21,13 +25,31 @@ class Player
   end
 
   def get_symbol
-    puts "Do you want to be \"X\" or \"O\""
+    puts "Player #{self.player_number}, do you want to be \"X\" or \"O\"?"
     @symbol = gets.chomp.upcase
+  end
+
+  def player_move
+    arr = []
+    puts "What ROW do you want to place your \"#{@symbol}\"?"
+    row = gets.chomp.to_i - 1
+    puts "What COLUMN do you want to place your \"#{@symbol}\"?"
+    column = gets.chomp.to_i - 1
+    arr << row << column
+    arr
   end
 end
 
+player1 = Player.new(1)
+puts "Player #{player1.player_number} is #{player1.symbol}"
+player2 = Player.new(2)
+puts "Player #{player2.player_number} is #{player2.symbol}"
+
+puts "Here's a look at the starting board."
 board = Board.new
 board.show_board
 
-player1 = Player.new(1)
-puts "Player #{player1.player_number} is #{player1.symbol}"
+location = player1.player_move
+board.place_choice(location)
+
+board.show_board
