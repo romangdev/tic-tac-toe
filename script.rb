7887ecac80a3856007296f18bf89b574c80ed570
Roman Geathers <romangeathersdev@gmail.com>
2022-06-15 11:59:09 -0400

    Add method to player that gets their symbol choice

--- script.rb
+++ script.rb
@@ -1,28 +1,33 @@
 class Board
-    attr_accessor :board_array
+	attr_accessor :board_array
 
-    def initialize 
-        @board_array = Array.new(3) {Array.new(3) {nil}}
-    end
+	def initialize 
+  	@board_array = Array.new(3) {Array.new(3) {nil}}
+  end
 
-    def show_board
-        for i in 0..2
-            p @board_array[i]
-        end
+  def show_board
+    for i in 0..2
+      p @board_array[i]
     end
-end
+  end
+end	
 
 class Player
-    def initialize(name)
-        @name = name
-        @symbol 
-    end
+  attr_reader :symbol, :player_number
 
-    def get_symbol
-        puts "Do you want to be \"X\" or \"O\""
-        @symbol = gets.chomp.upcase
-    end
+  def initialize(player_number)
+    @player_number = player_number
+    @symbol = self.get_symbol
+  end
+
+  def get_symbol
+    puts "Do you want to be \"X\" or \"O\""
+    @symbol = gets.chomp.upcase
+  end
 end
 
 board = Board.new
 board.show_board
+
+player1 = Player.new(1)
+puts "Player #{player1.player_number} is #{player1.symbol}"
